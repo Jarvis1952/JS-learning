7f72b0a7b4a8d1ccd7807955b81b26524328534a
NEEL <115575496+Jarvis1952@users.noreply.github.com>
2024-02-13 01:40:04 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,17 @@
+ let score = "33"
+// let score = null; // null 
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(score)
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33ABC" => NaN
+// true => 1, false => 0
+// 1 => true, 0 => false
+// "" => false
+// "someValue" => true
