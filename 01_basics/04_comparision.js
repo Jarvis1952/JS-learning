ca08278e77f7a09a73b200c66369543a3ac09fcd
NEEL <115575496+Jarvis1952@users.noreply.github.com>
2024-02-13 02:10:39 +0000

    comparison

--- 01_basics/04_comparision.js
+++
@@ -1,8 +0,0 @@
-console.log(null > 0); // false
-console.log(null == 0); // false
-console.log(null >= 0); // true
-
-// the reason is that an en equality check == and comparisons > < >= <= works differently 
-// Comparisons convert null to a number, treating it as 0 thats why null >= 0 is true and null > 0 is false
-
-// === strict check, check data types as well
