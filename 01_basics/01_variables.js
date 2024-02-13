1ebcffa1c12f03d714b2827c511fe00d940c7e7d
NEEL <115575496+Jarvis1952@users.noreply.github.com>
2024-02-13 01:24:35 +0000

    Variables and DataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 12312;
+let accountName = "neel@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 234 // not allowed
+
+/* prefer not to use var 
+ because of issue in block scope and functional scope */
+ 
+console.table([accountId, accountName, accountPassword, accountCity])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => value not defined
+// sybmol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined);  // undefined 
